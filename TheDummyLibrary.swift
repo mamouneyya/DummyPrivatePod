3a49f5d6876cf25179a3d0fe996171edfa441c75
Ma'moun Diraneyya <mamoun.diraneyya@gmail.com>
2016-03-02 01:31:01 +0200

    Try adding a private pod.

---
+++ TheDummyLibrary.swift
@@ -0,0 +1,17 @@
+//
+//  TheDummyLibrary.swift
+//  Dummy
+//
+//  Created by Ma'moun Diraneyya on 3/2/16.
+//  Copyright © 2016 Mamouneyy. All rights reserved.
+//
+
+import UIKit
+
+class TheDummyLibrary {
+
+    func foo() {
+        print("What the heck!")
+    }
+
+}
